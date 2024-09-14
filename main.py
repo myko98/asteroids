cbda9acd6e53899c15cfe945eaaa70cd2f9d8767
myko <michaelluchen1998@gmail.com>
2024-09-14 18:58:04 -0400

    Rotate player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -8,13 +9,28 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # Limits game to 60fps
+        # no code goes after this line
+        dt = clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
